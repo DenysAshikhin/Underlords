09a63af84445d75fe88e61620f6a3ecbaad72a96
Denys Ashikhin <denysashikhin@gmail.com>
2020-12-12 21:54:38 -0500

    Added bench sorting tiering up logic, configurable for any # of required units to tier up. Framework for bench logic in place.

--- code/botVision.py
+++ code/botVision.py
@@ -10,15 +10,10 @@ from PIL import ImageTk, Image
 from HUD import HUD
 from Shop import Shop
 
+from hero import Hero
 from pynput.mouse import Button, Controller as MouseController
 
 mouse1 = MouseController()
-
-
-
-
-
-
 
 
 def loadProfiles():
@@ -44,13 +39,14 @@ class ShopThread(Thread):
         self.bought = None
         self.shopChoices = None
         self.storeMap = [350, 450, 575, 700, 800]
+        self.purchaseHistory = []
 
         shopImages, classes, value, inspect, statesList = self.shop.labelShop()
 
         self.shopLabels = []
         self.shopImages = []
         self.benchLabels = []
-        self.benchLabelHero = [None, None, None, None, None, None, None, None]
+        self.benchHeroes = [None, None, None, None, None, None, None, None]
 
         self.hudLabel = None
         self.toBuy = None
@@ -112,20 +108,27 @@ class ShopThread(Thread):
                 if self.bought is not None:
                     for x in range(8):
 
-                        if self.benchLabelHero[x] is None:
-                            self.benchLabelHero[x] = classes[statesList[self.bought]]
+                        if self.benchHeroes[x] is None:
+                            self.benchHeroes[x] = Hero(classes[statesList[self.bought]], (x, -1),
+                                                       self.profilePics[classes[statesList[self.bought]]])
+
                             self.bought = None
-                            self.benchLabels[x].config(text=f"{self.benchLabelHero[x]}",
-                                                       image=self.profilePics[self.benchLabelHero[x]])
+                            self.benchLabels[x].config(text=f"{self.benchHeroes[x].name}",
+                                                       image=self.benchHeroes[x].image)
                             break
 
     def toggleStore(self, coords):
+
         mouse1.position = coords
         mouse1.click(Button.left, 1)
         time.sleep(1)
         self.shopChoices = self.shop.labelShop()
 
     def buy(self, xPos=350, idx=0):
+
+        if idx in self.purchaseHistory:
+            print("Invalid attempt to buy a unit!")
+            return -1
 
         hwnd = win32gui.FindWindow(None, 'Dota Underlords')
         win32gui.SetForegroundWindow(hwnd)
@@ -140,8 +143,96 @@ class ShopThread(Thread):
 
         mouse1.position = (x + xPos, y + 130)
         mouse1.click(Button.left, 1)
+
+        if self.benchLevelUp(idx):
+            self.bought = None
+            return 1
+
         self.bought = idx
 
+        # Expand this to take precedence for tiering up once it is relevant
+
+    def boardLevelUp(self):
+
+        levThresh = 2
+        # Adding +1 to represent the shop unit coming in
+        return {"tierTwo": 0, "tierOne": 0 + 1, "tierTwoHeroes": [], "tierOneHeroes": [], "tieredUp": False}
+
+    def benchLevelUp(self, idx):
+
+        levThresh = 2
+        tieredUp = False
+        shopImages, classes, value, inspect, statesList = self.shopChoices
+        boardScan = self.boardLevelUp()
+
+        if boardScan["tieredUp"] == True:
+            return True  # The shop unit will be consumed to tier up the units strictly on the board, bench is
+            # untouched - NOTE - note - make sure bench labels are not updated as a result of this in future
+            # make seperate function to update board labels!
+
+        bench = {"tierTwo": boardScan["tierTwo"], "tierOne": boardScan["tierOne"], "tierTwoHeroes": [],
+                 "tierOneHeroes": []}
+
+        for i in range(8):
+            if self.benchHeroes[i]:
+               
+                if self.benchHeroes[i].name == classes[statesList[idx]]:
+                    if self.benchHeroes[i].tier == 1:
+                        bench["tierOne"] += 1
+                        bench["tierOneHeroes"].append(self.benchHeroes[i])
+
+                    elif self.benchHeroes[i].tier == 2:
+                        bench["tierTwo"] += 1
+                        bench["tierTwoHeroes"].append(self.benchHeroes[i])
+
+        if bench["tierOne"] == levThresh:  # If there is enough tier ones to make a tier 2,
+            # first instance hero levels up, the rest should be removed from reference and update labels?
+
+            bench["tierOneHeroes"][0].tier += 1
+
+            if bench["tierOneHeroes"][0].coords[1] == -1:
+                self.benchLabels[bench["tierOneHeroes"][0].coords[0]].config(bg="blue")
+
+            bench["tierTwoHeroes"].append(bench["tierOneHeroes"][0])
+            bench["tierTwo"] += 1
+            tieredUp = True
+
+            for x in range(1, len(bench["tierOneHeroes"])):
+
+                specificHero = bench["tierOneHeroes"][x]
+
+                if specificHero.coords[1] == -1:  # Only if this hero is located on the bench!
+                    self.benchHeroes[specificHero.coords[0]] = None
+                    self.benchLabels[specificHero.coords[0]].config(text="Tiered Up")
+                # else
+                # To do - note - NOTE add board logic wiping later
+
+        if bench["tierTwo"] == levThresh:
+
+            bench["tierTwoHeroes"][0].tier += 1
+
+            if bench["tierTwoHeroes"][0].coords[1] == -1:
+                self.benchLabels[bench["tierTwoHeroes"][0].coords[0]].config(bg="yellow")
+
+            tieredUp = True
+
+            for x in range(1, len(bench["tierTwoHeroes"])):
+
+                specificHero = bench["tierTwoHeroes"][x]
+
+                if specificHero.coords[1] == -1:  # Only if this hero is located on the bench!
+                    self.benchHeroes[specificHero.coords[0]] = None
+                    self.benchLabels[specificHero.coords[0]].config(text="Tiered Up", bg='black')
+                # else
+                # To do - note - NOTE add board logic wiping later
+
+        #   print(bench)
+
+        return tieredUp
+
+    def updateBenchLabels(self):
+        # To do
+        return 1
 
 
 def openVision():
---
+++ code/hero.py
@@ -0,0 +1,7 @@
+class Hero:
+    def __init__(self, name, coords, image):
+
+        self.name = name
+        self.coords = coords
+        self.image = image
+        self.tier = 1
