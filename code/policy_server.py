1bed55697c35abe36ff1d330896a09e12313beb6
Denys Ashikhin <denysashikhin@gmail.com>
2021-05-04 00:57:43 -0400

    a

--- code/policy_server.py
+++ code/policy_server.py
@@ -84,7 +84,7 @@ DEFAULT_CONFIG = with_common_config({
     # Initial coefficient for KL divergence.
     "kl_coeff": 0.2,
     # Size of batches collected from each worker.
-    "rollout_fragment_length": 25,
+    "rollout_fragment_length": 50,
     # Number of timesteps collected for each SGD round. This defines the size
     # of each SGD epoch.
     "train_batch_size": 2000,
