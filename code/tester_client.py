b90add1e9d44b82a3e0c84d0b11189b3e1d1876c
Denys Ashikhin <denysashikhin@gmail.com>
2021-07-16 12:57:07 -0400

    Update tester_client.py

--- code/tester_client.py
+++ code/tester_client.py
@@ -1,14 +1,12 @@
 from ray.rllib.env import PolicyClient
-
-from environment import UnderlordEnv
+from ray.rllib.examples.env.random_env import RandomEnv
 import logging
 import time
 import argparse
 from six.moves import queue
+from gym import spaces
 
-from logger import logger
-
-logging.basicConfig(level=logging.INFO)
+#logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser(description='Optional app description')
 parser.add_argument('-ip', type=str,
@@ -36,37 +34,89 @@ if args.update:
 if args.local:
     local = args.local
 
+
 print(f"Going to update at {update} seconds interval")
 
 print('trying to launch policy client')
-client = PolicyClient(address=f"http://{args.ip}:55555", update_interval=None, inference_mode=local)
+client = PolicyClient(address=f"http://{args.ip}:55555", update_interval=update, inference_mode=local)
 
-# for testing purposes, we will force the env ceated in policy client+server to be a random one and use our game wrapper
-# for getting the observation/action/reward
 
-# local = 'remote'
+#for testing purposes, we will force the env ceated in policy client+server to be a random one and use our game wrapper
+#for getting the observation/action/reward
+
+#local = 'remote'
 forced = True
 
-if local == 'remote':
-    env = UnderlordEnv({'sleep': True})
-else:
-    env = client.env
+env_config = {}
+env_config["observation_space"] = spaces.Tuple(
+        (spaces.Discrete(9),  # final position * (if not 0 means game is over!)
+         spaces.Discrete(101),  # health *
+         spaces.Discrete(100),  # gold
+         spaces.Discrete(11),  # level *
+         spaces.Discrete(99),  # remaining EXP to level up
+         spaces.Discrete(50),  # round
+         spaces.Discrete(2),  # locked in
+         spaces.Discrete(6),  # gamePhase *
+         spaces.MultiDiscrete([250, 3]),  # heroToMove: heroLocalID, isUnderlord
+         spaces.Discrete(250),  # itemToMove: localID*,
+         spaces.Discrete(3),  # reRoll cost
+         spaces.Discrete(2),  # rerolled (item)
+         spaces.Discrete(35),  # current round timer
+         # below are the store heros
+         spaces.MultiDiscrete([71, 71, 71, 71, 71]),
+         # below are the bench heroes
+         spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
+         # below are the board heros
+         spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]), spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]), spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]), spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]), spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]),
+         spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]), spaces.MultiDiscrete([71, 14, 4, 6, 250, 9, 9, 3]),
+         # below are underlords to pick (whenever valid) -> underlord ID - specialty
+         spaces.MultiDiscrete([5, 3, 5, 3, 5, 3, 5, 3]),
+         # below are the items
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+         # below are the items to pick from
+         spaces.MultiDiscrete([70, 70, 70]),
+         # below are dicts of other players: slot, health, gold, level, boardUnits (ID, Tier)
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
+         spaces.MultiDiscrete(
+             [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4])
+
+         ))
+env_config["action_space"] = spaces.MultiDiscrete([9, 9, 9, 4])
+env_config["p_done"] = 0.05
+env = RandomEnv(env_config)
+
 
 print('trying to get initial eid')
 episode_id = client.start_episode()
 
-if local == 'remote':
-    env.underlord.startNewGame()
-
+gameObservation = env.reset()
 reward = 0
 print('starting main loop')
 
+
 while True:
-
-    gameObservation = env.underlord.getObservation()
-
-    env.root.update()  # updating gui (tkinter) to see what the bot sees of the game state
-
     action = None
 
     for i in range(10):
@@ -81,27 +131,24 @@ while True:
     if action is None:
         raise ValueError("Policy failed to return an action after 10 tries")
 
-    reward += env.underlord.act(action=action[0], x=action[1] - 1, y=action[2] - 1, selection=action[3] - 1)
+    gameObservation, reward, done, info = env.step([action[0], action[1], action[2], action[3]])
 
     client.log_returns(episode_id=episode_id, reward=reward)
 
-    finalPosition = env.underlord.finalPlacement
+    finalPosition = int(done) #env.underlord.finalPlacement
 
     if finalPosition != 0:
         print(f"GAME OVER! final position: {finalPosition} - final reward: {reward}")
         reward = 0
         # need to call a reset of env here
         client.end_episode(episode_id=episode_id, observation=gameObservation)
-        env.underlord.resetEnv()
 
         # if forced:
         #     print("Updating policy weights")
         #     client.update_policy_weights()
 
         episode_id = client.start_episode(episode_id=None)
-
-        if local == 'remote':
-            env.underlord.startNewGame()
+        gameObservation = env.reset()
         print('got past restarting of the new episode, for loop should begin anew!')
 
     print('----')
