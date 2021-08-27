e42a1669237b797afdea1a839b74021824732184
Denys Ashikhin <denysashikhin@gmail.com>
2021-08-27 19:58:54 -0400

    Update policy_server.py

--- code/policy_server.py
+++ code/policy_server.py
@@ -29,16 +29,16 @@ DEFAULT_CONFIG = with_common_config({
     # Initial coefficient for KL divergence.
     "kl_coeff": 0.2,
     # Size of batches collected from each worker.
-    "rollout_fragment_length": 1000,
+    "rollout_fragment_length": 100,
     # Number of timesteps collected for each SGD round. This defines the size
     # of each SGD epoch.
     "train_batch_size": 2000,
     # Total SGD batch size across all devices for SGD. This defines the
     # minibatch size within each epoch.
-    "sgd_minibatch_size": 500,
+    "sgd_minibatch_size": 200,
     # Number of SGD iterations in each outer loop (i.e., number of epochs to
     # execute per train batch).
-    "num_sgd_iter": 25,
+    "num_sgd_iter": 50,
     # Whether to shuffle sequences in the batch when training (recommended).
     "shuffle_sequences": True,
     # Stepsize of SGD.
@@ -52,8 +52,8 @@ DEFAULT_CONFIG = with_common_config({
         # Share layers for value function. If you set this to True, it's
         # important to tune vf_loss_coeff.
         "vf_share_layers": False,
-        "fcnet_hiddens": [64, 64],
-        "use_lstm": True,
+        "fcnet_hiddens": [256, 256],
+        "use_lstm": False,
         "max_seq_len": 2,
         "lstm_cell_size": 128
     },
@@ -99,15 +99,15 @@ DEFAULT_CONFIG = with_common_config({
         "type": "Curiosity",  # <- Use the Curiosity module for exploring.
         "eta": 1.0,  # Weight for intrinsic rewards before being added to extrinsic ones.
         "lr": 0.001,  # Learning rate of the curiosity (ICM) module.
-        "feature_dim": 256,  # Dimensionality of the generated feature vectors.
+        "feature_dim": 512,  # Dimensionality of the generated feature vectors.
         # Setup of the feature net (used to encode observations into feature (latent) vectors).
         "feature_net_config": {
             "fcnet_hiddens": [],
             "fcnet_activation": "relu",
         },
-        "inverse_net_hiddens": [64],  # Hidden layers of the "inverse" model.
+        "inverse_net_hiddens": [128],  # Hidden layers of the "inverse" model.
         "inverse_net_activation": "relu",  # Activation of the "inverse" model.
-        "forward_net_hiddens": [64],  # Hidden layers of the "forward" model.
+        "forward_net_hiddens": [128],  # Hidden layers of the "forward" model.
         "forward_net_activation": "relu",  # Activation of the "forward" model.
         "beta": 0.2,  # Weight for the "forward" loss (beta) over the "inverse" loss (1.0 - beta).
         # Specify, which exploration sub-type to use (usually, the algo's "default"
