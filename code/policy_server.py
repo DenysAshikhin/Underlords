5c10a25ab3260eaba34a8b83a32408c7951e5dfd
Denys Ashikhin <denysashikhin@gmail.com>
2021-09-05 16:30:00 -0400

    Update policy_server.py

--- code/policy_server.py
+++ code/policy_server.py
@@ -32,13 +32,13 @@ DEFAULT_CONFIG = with_common_config({
     "rollout_fragment_length": 20,
     # Number of timesteps collected for each SGD round. This defines the size
     # of each SGD epoch.
-    "train_batch_size": 5000,
+    "train_batch_size": 3000,
     # Total SGD batch size across all devices for SGD. This defines the
     # minibatch size within each epoch.
-    "sgd_minibatch_size": 100,
+    "sgd_minibatch_size": 500,
     # Number of SGD iterations in each outer loop (i.e., number of epochs to
     # execute per train batch).
-    "num_sgd_iter": 50,
+    "num_sgd_iter": 25,
     # Whether to shuffle sequences in the batch when training (recommended).
     "shuffle_sequences": True,
     # Stepsize of SGD.
@@ -52,7 +52,7 @@ DEFAULT_CONFIG = with_common_config({
         # Share layers for value function. If you set this to True, it's
         # important to tune vf_loss_coeff.
         "vf_share_layers": False,
-        "fcnet_hiddens": [16, 16],
+        "fcnet_hiddens": [20, 20],
         "use_lstm": False
         # "max_seq_len": 3,
     },
@@ -104,9 +104,9 @@ DEFAULT_CONFIG = with_common_config({
             "fcnet_hiddens": [],
             "fcnet_activation": "relu",
         },
-        "inverse_net_hiddens": [16],  # Hidden layers of the "inverse" model.
+        "inverse_net_hiddens": [20],  # Hidden layers of the "inverse" model.
         "inverse_net_activation": "relu",  # Activation of the "inverse" model.
-        "forward_net_hiddens": [16],  # Hidden layers of the "forward" model.
+        "forward_net_hiddens": [20],  # Hidden layers of the "forward" model.
         "forward_net_activation": "relu",  # Activation of the "forward" model.
         "beta": 0.2,  # Weight for the "forward" loss (beta) over the "inverse" loss (1.0 - beta).
         # Specify, which exploration sub-type to use (usually, the algo's "default"
