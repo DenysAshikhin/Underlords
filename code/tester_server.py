c1dd3d828400b57368d0c439e4576d46ba3ad0fc
Denys Ashikhin <denysashikhin@gmail.com>
2021-09-12 22:05:47 -0400

    Update tester_server.py

--- code/tester_server.py
+++ code/tester_server.py
@@ -120,67 +120,78 @@ DEFAULT_CONFIG = with_common_config({
 
 })
 
+heroId = 72
 DEFAULT_CONFIG["env_config"]["observation_space"] = spaces.Tuple(
-    (spaces.Discrete(9),  # final position * (if not 0 means game is over!)
-     spaces.Discrete(101),  # health *
-     spaces.Discrete(100),  # gold
-     spaces.Discrete(11),  # level *
-     spaces.Discrete(99),  # remaining EXP to level up
-     spaces.Discrete(50),  # round
-     spaces.Discrete(2),  # locked in
-     spaces.Discrete(6),  # gamePhase *
-     spaces.MultiDiscrete([250, 3]),  # heroToMove: heroLocalID, isUnderlord
-     spaces.Discrete(250),  # itemToMove: localID*,
-     spaces.Discrete(3),  # reRoll cost
-     spaces.Discrete(2),  # rerolled (item)
-     spaces.Discrete(35),  # current round timer
-     # below are the store heros
-     spaces.MultiDiscrete([71, 71, 71, 71, 71]),
-     # below are the bench heroes
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     # below are the board heros
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([71, 250, 4, 6, 14, 9, 9, 3]),
-     # below are underlords to pick (whenever valid) -> underlord ID - specialty
-     spaces.MultiDiscrete([5, 3, 5, 3, 5, 3, 5, 3]),
-     # below are the items
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
-     # below are the items to pick from
-     spaces.MultiDiscrete([70, 70, 70]),
-     # below are dicts of other players: slot, health, gold, level, boardUnits (ID, Tier)
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4]),
-     spaces.MultiDiscrete(
-         [9, 101, 100, 11, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4, 71, 4])
-
-     ))
-DEFAULT_CONFIG["env_config"]["action_space"] = spaces.MultiDiscrete([9, 9, 9, 4])
+            (spaces.Discrete(9),  # final position * (if not 0 means game is over!)
+             spaces.Discrete(101),  # health *
+             spaces.Discrete(100),  # gold
+             spaces.Discrete(11),  # level *
+             spaces.Discrete(99),  # remaining EXP to level up
+             spaces.Discrete(50),  # round
+             spaces.Discrete(2),  # locked in
+             spaces.Discrete(2),  # punish for locking in this round
+             spaces.Discrete(6),  # gamePhase *
+             spaces.MultiDiscrete([250, 3]),  # heroToMove: heroLocalID, isUnderlord
+             spaces.Discrete(250),  # itemToMove: localID*,
+             spaces.Discrete(3),  # reRoll cost
+             spaces.Discrete(2),  # rerolled (item)
+             spaces.Discrete(35),  # current round timer
+             # below are the store heros
+             spaces.MultiDiscrete([heroId, heroId, heroId, heroId, heroId]),
+             # below are the bench heroes
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             # below are the board heros
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]), spaces.MultiDiscrete([heroId, 250, 4, 6, 14, 9, 9, 3]),
+             # below are underlords to pick (whenever valid) -> underlord ID - specialty
+             spaces.MultiDiscrete([5, 3, 5, 3, 5, 3, 5, 3]),
+             # below are the items
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             spaces.MultiDiscrete([70, 14, 250, 4, 5]), spaces.MultiDiscrete([70, 14, 250, 4, 5]),
+             # below are the items to pick from
+             spaces.MultiDiscrete([70, 70, 70]),
+             # below are dicts of other players: slot, health, gold, level, boardUnits (ID, Tier)
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4]),
+             spaces.MultiDiscrete(
+                 [9, 101, 100, 11, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4, heroId, 4])
+             ))
+DEFAULT_CONFIG["env_config"]["action_space"] = spaces.MultiDiscrete([7, 9, 9])
 
 ray.init()
 trainer = PPOTrainer(config=DEFAULT_CONFIG, env=RandomEnv)
 
-# Serving and training loop.
-i = 0
-while True:
-    print(pretty_print(trainer.train()))
+checkpoint_path = "checkpointsA/"
+checkpoint1 = "checkpoint"
+
+print(args.checkpoint)
+
+if args.checkpoint:
+    # Attempt to restore from checkpoint, if possible.
+    if os.path.exists(args.checkpoint):
+        print('path FOUND!')
+        print("Restoring from checkpoint path", args.checkpoint)
+        trainer.restore(args.checkpoint)
+    else:
+        print("That path does not exist!")
+
