fd7880aaf7d0f6e32106527e81de6d227526d663
Denys Ashikhin <denysashikhin@gmail.com>
2021-10-24 01:38:25 -0400

    Update botVision.py

--- code/botVision.py
+++ code/botVision.py
@@ -1811,16 +1811,16 @@ class UnderlordInteract():
             self.heroToMove = None
         else:
 
-            if self.boardHeroes[x][y] is None:
+            if self.boardHeroes[y][x] is None:
                 self.mediumPunish = True
                 return -1
 
             mouse1.position = (self.boardX + (self.boardXOffset * y), self.boardY + (self.boardYOffset * x))
-            if self.boardHeroes[x][y].item is not None:
-                self.boardHeroes[x][y].item.hero = None
+            if self.boardHeroes[y][x].item is not None:
+                self.boardHeroes[y][x].item.hero = None
 
-            earnedMoney = self.boardHeroes[x][y].gold + (self.boardHeroes[x][y].tier - 1) * 2
-            self.resetLabel(self.boardHeroes[x][y])
+            earnedMoney = self.boardHeroes[y][x].gold + (self.boardHeroes[y][x].tier - 1) * 2
+            self.resetLabel(self.boardHeroes[y][x])
             self.heroToMove = None
         # print(f"Moving to board {mouse1.position}")
 
